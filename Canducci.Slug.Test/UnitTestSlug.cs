c56de16c3c37b52e8a19f16cbe4aa7d0b1a45ded
fulviocanducci <fulviocanducci@hotmail.com>
2021-05-17 10:16:50 -0300

    Add Test

--- Canducci.Slug.Test/UnitTestSlug.cs
+++ Canducci.Slug.Test/UnitTestSlug.cs
@@ -1,15 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Canducci.Slug.Test
 {
-  [TestClass]
-  public class UnitTestSlug
-  {
-    [TestMethod]
-    public void TestSlugTextValue()
+    [TestClass]
+    public class UnitTestSlug
     {
-      string text = "Friend Sôuza";
-      string slug = text.ToSlug();
-      Assert.AreEqual("friend-souza", slug);
+        [TestMethod]
+        public void TestSlugTextValue()
+        {
+            string text = "Friend Sôuza";
+            string slug = text.ToSlug();
+            Assert.AreEqual("friend-souza", slug);
+        }
+
+        [TestMethod]
+        public void TestSlugExtension()
+        {
+            string result = SlugExtensions.ToSlug("Friend Item 45 Friend");
+            Assert.AreEqual(result, "friend-item-45-friend");
+        }
+
+        [TestMethod]
+        public void TestSlugExtensionLenthChar()
+        {
+            string result = SlugExtensions.ToSlug("Friend Item 45 Friend", 10);
+            Assert.AreEqual(result, "friend-ite");
+        }
+
+        [TestMethod]
+        public void TestCountChar()
+        {
+            string result = "product source main".ToSlug(15);
+            Assert.AreEqual(result, "product-source");
+            Assert.AreEqual(14, result.Length);
+        }
     }
-  }
 }
